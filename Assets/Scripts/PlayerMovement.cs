2e45555cc8396026e99adbbc0d8d28db42c349b8
krstevskinikola173-droid <krstevskinikola173@gmail.com>
2025-10-06 17:06:18 +0200

    Prv cas

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space))
+        body.velocity = new Vector2(body.velocity.x, Speed);
+    }
+}
