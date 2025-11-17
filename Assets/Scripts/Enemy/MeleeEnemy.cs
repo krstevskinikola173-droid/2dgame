7c51af30a734fd308241bc559bf0291a8ce9621e
krstevskinikola173-droid <krstevskinikola173@gmail.com>
2025-11-17 16:39:11 +0100

    5ti chas
    Dodavanje na poveke traps sho prat damage i dodavanje na enemy so melee i ranged attack (mozhi da se menva range, attack dmg)

---
+++ Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private int range;
+    [SerializeField] private int colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight
+        if(PlayerInSight())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                //Attack
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z) boxCollider.bounds.size, 
+            0, Vector2.left, 0, playerLayer);
+
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x,
+        new Vector3(boxCollider.bounds.size * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+}
